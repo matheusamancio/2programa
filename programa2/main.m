a36d3cd49bf532756573add8ccb9160c6db84c82
Matheus Amancio Seixeiro <matheusamancio23@hotmail.com>
2015-02-04 11:49:36 -0200

    Initial Commit

---
+++ programa2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  programa2
+//
+//  Created by Matheus Amancio Seixeiro on 04/02/15.
+//  Copyright (c) 2015 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
